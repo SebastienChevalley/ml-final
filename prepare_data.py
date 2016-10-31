b7de208699b1d8bfafbcf5c1234fc0e853cf6b69
Sébastien Chevalley <sebastien.chevalley@epfl.ch>
2016-10-31 17:31:50 +0100

    add features_list support as argument

--- prepare_data.py
+++ prepare_data.py
@@ -5,20 +5,21 @@ Feature processing
 
 from model import *
 
-def prepare_data(raw_tx_train, y_train, raw_tx_test= None):
+def prepare_data(raw_tx_train, y_train, features_list, raw_tx_test= None):
     """
     Load an prepare data matrix,
     returns the tx matrix for training and testing set (if provided)
     """
+        
     print("Building new dataset (1/3)...")
-    intermediate_weights, intermediate_windows, ack = trainprocess(raw_tx_train, y_train)
+    intermediate_weights, intermediate_windows, ack = trainprocess(raw_tx_train, y_train, features_list)
     print("Building new dataset (2/3)...")
-    tx_train_edited = predictprocess(raw_tx_train, intermediate_weights, intermediate_windows, ack)
+    tx_train_edited = predictprocess(raw_tx_train, intermediate_weights, intermediate_windows, ack, features_list)
 
     tx_test_edited = None
     if raw_tx_test is not None:
         print("Building new dataset (3/3)...")
-        tx_test_edited = predictprocess(raw_tx_test, intermediate_weights, intermediate_windows, ack)
+        tx_test_edited = predictprocess(raw_tx_test, intermediate_weights, intermediate_windows, ack, features_list)
     else:
         print("Building new dataset (3/3) [skipped]...")
 
--- run.py
+++ run.py
@@ -16,7 +16,7 @@ from helpers import *
 from proj1_helpers import *
 from cross_validation import cross_validation
 from prepare_data import prepare_data
-
+import os.path
 
 if __name__ == "__main__":
 
@@ -25,26 +25,43 @@ if __name__ == "__main__":
 	DATA_TRAIN_PATH = 'train.csv'
 	DATA_TEST_PATH = 'test.csv'
 
-	_, raw_tx_test, ids_test = load_csv_data(DATA_TEST_PATH)
-	y_train, raw_tx_train, ids_train = load_csv_data(DATA_TRAIN_PATH)
-	## ================================================ ##
+	if True or not os.path.isfile("tx_train_edited.npy"):
+		_, raw_tx_test, ids_test = load_csv_data(DATA_TEST_PATH)
+		y_train, raw_tx_train, ids_train = load_csv_data(DATA_TRAIN_PATH)
+		## ================================================ ##
 
 
-	## ========= Preprocess data =============================== ##
-	print("Preprocessing data...")
-	tx_train_edited, tx_test_edited = prepare_data(raw_tx_train, y_train, raw_tx_test)
-	## ========================================================= ##
+		## ========= Preprocess data =============================== ##
+		print("Preprocessing data...")
+		features_list = [[i] for i in range(13)]
+		tx_train_edited, tx_test_edited = prepare_data(raw_tx_train, y_train, features_list, raw_tx_test)
+		## ========================================================= ##
+		np.save("ids_test", ids_test)
+		np.save("y_train", y_train)
+		np.save("tx_train_edited", tx_train_edited)
+		np.save("tx_test_edited", tx_test_edited)
+		np.save("raw_tx_train", raw_tx_train)
+		np.save("raw_tx_test", raw_tx_test)
+	else:
+		print("Preprocessing data (from cache)...")
+		ids_test = np.load("ids_test.npy")
+		y_train = np.load("y_train.npy")
+		tx_train_edited = np.load("tx_train_edited.npy")
+		tx_test_edited = np.load("tx_test_edited.npy")
+		raw_tx_train = np.load("raw_tx_train.npy")
+		raw_tx_test = np.load("raw_tx_test.npy")
 
 
+	
 	## ========= Train and validate model using cross validation ========== ##
-	model = 'logistic_regression'   ## 'least_squares','ridge_regression','logistic_regression','reg_logistic_regression'
-	K = 2                           ## K fold cross validation
+	model = 'least_squares'   ## 'least_squares','ridge_regression','logistic_regression','reg_logistic_regression'
+	K = 10                           ## K fold cross validation
 	gamma = 0.01                    ## Learning rate for gradient descent
 	lambda_ = 0.01                  ## Regularisation parameter
 	max_iters = 10                  ## Maximum number of iteration
 	initial_w = np.zeros(tx_train_edited.shape[1])# Weight initialisation
 	print("Cross validation with model: ", model)
-	# Invoke the cross validation function with the above model 
+	# Invoke the cross validation function with the above model
 	w, loss, avg_val_err, avg_train_err = cross_validation(y_train, tx_train_edited, K, model, lambda_, gamma,
 	                                     max_iters, initial_w, num_epochs=1, shuffle=True, seed=1)
 	## ==================================================================== ##
@@ -79,6 +96,7 @@ if __name__ == "__main__":
 	OUTPUT_PATH = 'result.csv'
 	create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
     ## ============================================== ##
+
 
 
 
